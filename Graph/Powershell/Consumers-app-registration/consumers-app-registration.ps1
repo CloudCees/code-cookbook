5b68841d4a2b18c5bef8d73e230f3deac8fe1374
Cees van Altena <ceesvana@hotmail.com>
2024-12-07 12:40:25 +0100

    Added first-level in depth search

--- Graph/Powershell/Consumers-app-registration/consumers-app-registration.ps1
+++ Graph/Powershell/Consumers-app-registration/consumers-app-registration.ps1
@@ -1,32 +1,59 @@
 [CmdletBinding()]
 <#
 .SYNOPSIS
-Script to map the consumers of an application registration.
+This script registers a consumer application in Azure AD.
 
 .PARAMETER TargetAppId
-Define the client ID of the app registration you are interested in.
+Specifies the target application ID for the consumer app registration. This parameter is mandatory.
+
+.PARAMETER InDepthSearch
+Indicates whether to perform an in-depth search. This is an optional switch parameter with a default value of $false.
+
 #>
-param (
-    [Parameter(Mandatory)]
-    [string]
-    $TargetAppId
-)
+        [array]$allApps
+    )
+    $filteredApps = $allApps | Where-Object {
+        $_.RequiredResourceAccess.ResourceAppId -match $appId
+    }    
+
+    return $filteredApps
+}
 
 Connect-MgGraph
 
 # Get all app registrations
 $appRegistrations = Get-MgApplication -All 
-$filteredApps = $appRegistrations | Where-Object {
-    $_.RequiredResourceAccess.ResourceAppId -match $TargetAppId
-}
 
 # Output the target app information
 $appRegistrations | Where-Object {
     $_.AppId -match $TargetAppId
 } | Select-Object DisplayName, AppId 
 
+$consumers = Get-Consumers -appId $TargetAppId -allApps $appRegistrations
+
 Write-Output ""
 Write-Output "Is used by:"
 
+if (!$InDepthSearch) {
+    $consumers | Select-Object DisplayName, AppId
+    return
+}
+
 # Output the filtered app registrations
-$filteredApps | Select-Object DisplayName, AppId
+foreach ($consumer in $consumers) {
+    $subConsumers = Get-Consumers -appId $consumer.AppId -allApps $appRegistrations
+
+    Write-Output ""
+    Write-Output $consumer
+
+    if ($subConsumers.Count -gt 0) {
+        Write-Output ""
+        Write-Output "Which is used by:"
+
+        $subConsumers | Select-Object DisplayName, AppId
+    }
+    else {
+        Write-Output ""
+        Write-Output "No consumers"
+    }
+}
